35b11de4a6a9cbe95443072931c2c188e20ac71f
KD131 <71063280+KD131@users.noreply.github.com>
2021-11-11 14:04:14 +0100

    adds note to GeekJokeDTO explaining that it's a terrible API

--- src/main/java/dtos/funstuff/GeekJokeDTO.java
+++ src/main/java/dtos/funstuff/GeekJokeDTO.java
@@ -1,7 +1,9 @@
 package dtos.funstuff;
 
 public class GeekJokeDTO {
-    // Almost always gives a Chuck joke, which is not a geek joke, but I didn't make the API.
+    private String note =
+            "Almost always gives a Chuck joke, which is not a geek joke, but I didn't make the API. " +
+                    "It'd be way better to fetch a programming joke from https://v2.jokeapi.dev/joke/Programming?type=single&format=json.";
     private String joke;
 
     public GeekJokeDTO() {
